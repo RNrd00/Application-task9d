228d5a6f28fb54ad8624204c7143eee74532e1bc
RNrd00 <117254446+RNrd00@users.noreply.github.com>
2023-01-15 23:54:02 +0000

    [Finish]9d

--- Bookers7d~9d/app/controllers/books_controller.rb
+++ Bookers7d~9d/app/controllers/books_controller.rb
@@ -8,22 +8,16 @@ class BooksController < ApplicationController
   end
 
   def index
-    if params[:latest]
-     @books = Book.latest
-    elsif params[:old]
-     @books = Book.old
-    elsif params[:rate_count]
-     @books = Book.star_count
-    else
-     @books = Book.all
-    end
-     @book = Book.new
+    @books = Book.all.order(params[:sort])
+    @book = Book.new
   end
 
   def create
     @book = Book.new(book_params)
     @book.user_id = current_user.id
+    tag_list = params[:book][:tag_name].split(',')
     if @book.save
+      @book.save_tags(tag_list)
       redirect_to book_path(@book), notice: "You have created book successfully."
     else
       @books = Book.all
